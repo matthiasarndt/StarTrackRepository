e0f5c1e71a17cc564cc9df1384df63e44cdd8e8e
Matthias Arndt <98777913+matthiasarndt@users.noreply.github.com>
2026-01-19 16:12:47 +0100

    0.3.0

--- main.py
+++ main.py
@@ -22,13 +22,13 @@ if __name__ == "__main__":
     run_dir = os.path.dirname(os.path.abspath(__file__))
     data_dir = Path(run_dir) / "raw_data_horsehead"
     n_aligning_stars = 5  # recommended to be 5, maximum value is 9
-    t_value = 230  # manually set this based on the brightness of the starfield, setting it to -1 will make StarTrack tune the value
+    t_value = 240  # manually set this based on the brightness of the starfield, setting it to -1 will make StarTrack tune the value
 
     # determine how many cores to use, in the example below n-1 available physical cores are used.
     n_physical_cores = psutil.cpu_count(logical=False)
     n_cores = max(1, n_physical_cores - 1)
 
     # create image object from astrophoto
-    m82_data = FrameStack(data_directory=data_dir, n_aligning_stars=n_aligning_stars, verbosity=0, ref_frame_name="L_0099.NEF", threshold=-1, max_cores=4)
+    m82_data = FrameStack(data_directory=data_dir, n_aligning_stars=n_aligning_stars, verbosity=0, ref_frame_name="L_0099.NEF", threshold=t_value, max_cores=4)
     m82_data.compute_stack()
     m82_data.convert_to_stacked_image()
